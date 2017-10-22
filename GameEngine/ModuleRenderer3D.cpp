3694e0c3bb85fe6a48f930caf75357a06e3893bc
Pau5erra <pau.serra11@hotmail.com>
2017-10-22 17:24:49 +0200

    Commented line

--- GameEngine/ModuleRenderer3D.cpp
+++ GameEngine/ModuleRenderer3D.cpp
@@ -239,6 +239,7 @@ void ModuleRenderer3D::DrawGameObject(GameObject* go)
 			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, (mesh)->buffIndex);
 			glDrawElements(GL_TRIANGLES, (mesh)->numIndex, GL_UNSIGNED_INT, NULL);
 		}
+		//float3 hola = ((cTransform*)mesh->gameObject->FindComponent(TRANSFORM))->positionLocal;
 		// CleanUp
 		glBindTexture(GL_TEXTURE_2D, 0);
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
