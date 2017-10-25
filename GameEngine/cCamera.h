51d03a6cdde991732aa4385000b6064c15e865e1
Pau5erra <pausc1@TR12.CAT>
2017-10-25 12:03:49 +0200

    Begining of component camera

--- GameEngine/cCamera.h
+++ GameEngine/cCamera.h
@@ -1,24 +1,15 @@
 #include "Component.h"
 #include "MathGeoLib/MathGeoLib.h"
 
-
+class cTransform;
 class cCamera : public Component
 {
 public:
 	cCamera(GameObject* _gameObject);
 	~cCamera();
 
-	std::vector<float3> normals;
-	std::vector<float3> vertex;
-
-	uint buffNormals = 0;
-	uint buffUv = 0;
-	uint buffVertex = 0;
-	uint buffIndex = 0;
-	uint numIndex = 0;
-	uint buffTexture = 0;
-
-	AABB aabbBox;
+	math::Frustum frustum;
+	cTransform* transform;
 
 	void RealUpdate()
 	{
