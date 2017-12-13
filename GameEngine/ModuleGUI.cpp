fbf97147dd71c0741368b2331b6fd2c68a511919
Ruben Sardon <ec4who@hotmail.com>
2017-12-13 23:02:04 +0100

    GUI - Added Debug Features
    Space = blank Image
    F = reduce canvas scale x
    G = reduce canvas scale y
    R = increase canvas scale x
    T = increase canvas scale y

--- GameEngine/ModuleGUI.cpp
+++ GameEngine/ModuleGUI.cpp
@@ -2,6 +2,7 @@
 #include "Application.h"
 #include "ModuleWindow.h"
 #include "ModuleScene.h"
+#include "ModuleInput.h"
 #include "cTransform.h"
 #include "UIComponents.h"
 
@@ -23,6 +24,34 @@ bool ModuleGUI::Start()
 }
 update_status ModuleGUI::Update(float dt)
 {
+	if (App->input->GetKey(SDL_SCANCODE_SPACE) == KEY_DOWN)
+		AddUIImage(nullptr);
+
+	if (App->input->GetKey(SDL_SCANCODE_F) == KEY_DOWN)
+	{
+		GTI::UIElement* tmp = canvas->GetUI();
+		GTI::RectTransform* rect = tmp->transform;
+		rect->scaleLocal.x *= 0.5;
+	}
+	if (App->input->GetKey(SDL_SCANCODE_G) == KEY_DOWN)
+	{
+		GTI::UIElement* tmp = canvas->GetUI();
+		GTI::RectTransform* rect = tmp->transform;
+		rect->scaleLocal.y *= 0.5;
+	}
+	if (App->input->GetKey(SDL_SCANCODE_R) == KEY_DOWN)
+	{
+		GTI::UIElement* tmp = canvas->GetUI();
+		GTI::RectTransform* rect = tmp->transform;
+		rect->scaleLocal.x *= 1.5;
+	}
+	if (App->input->GetKey(SDL_SCANCODE_T) == KEY_DOWN)
+	{
+		GTI::UIElement* tmp = canvas->GetUI();
+		GTI::RectTransform* rect = tmp->transform;
+		rect->scaleLocal.y *= 1.5;
+	}
+
 	return UPDATE_CONTINUE;
 }
 
@@ -46,9 +75,7 @@ bool ModuleGUI::AddUIImage(char* path)
 	}
 
 	GameObject* image_go = App->scene->CreateGameObject("Image", true, canvas->gameObject, true);
-	cImage* image = new cImage(image_go);
+	cImage* image = new cImage(image_go, path);
 	((cTransform*)image_go->FindComponent(TRANSFORM))->SetRectSource(image);
-
-	image->image->buffTexture = GTI::GTInterface.LoadTexture(path);
-	return image->image->buffTexture != 0;
+	return image->GetUI()->buffTexture != 0;
 }
